18459ae618a4584f4beb6a4c103203a3a2f67c8c
gregdavies <gd3@princeton.edu>
2025-11-21 03:40:40 +0000

    added iodevices_lwp3

--- bricks/movehub/mpconfigport.h
+++ bricks/movehub/mpconfigport.h
@@ -35,6 +35,7 @@
 // ------------------------------------------------------------
 #define PYBRICKS_PY_HUBS                        (1)
 #define PYBRICKS_PY_IODEVICES                   (1)
+#define PYBRICKS_PY_IODEVICES_LWP3              (1)
 #define PYBRICKS_PY_PARAMETERS                  (1)
 #define PYBRICKS_PY_PARAMETERS_BUTTON           (1)
 #define PYBRICKS_PY_PARAMETERS_BUTTON_REMOTE_ONLY (0)
