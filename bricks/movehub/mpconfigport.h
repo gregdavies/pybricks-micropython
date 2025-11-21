be98c82dbb8ff9c52ebb777d44813ea8f195d4e8
gregdavies <gd3@princeton.edu>
2025-11-21 03:30:28 +0000

    reenabled common_system

--- bricks/movehub/mpconfigport.h
+++ bricks/movehub/mpconfigport.h
@@ -26,7 +26,7 @@
 #define PYBRICKS_PY_COMMON_LOGGER               (0)
 #define PYBRICKS_PY_COMMON_MOTOR_MODEL          (0)
 #define PYBRICKS_PY_COMMON_MOTORS               (1)
-#define PYBRICKS_PY_COMMON_SYSTEM               (0)
+#define PYBRICKS_PY_COMMON_SYSTEM               (1)
 
 #define PYBRICKS_PY_EXPERIMENTAL                (0)
 
